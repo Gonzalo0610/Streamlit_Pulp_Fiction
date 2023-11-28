c411d897caa21ae8cef120810f6d62a9d09223d1
Gonzalo Quesada <gqsalvat@gmail.com>
2023-11-28 09:02:47 +0100

    Upload

--- pages/07 Pulp Fiction Facts.py
+++ pages/07 Pulp Fiction Facts.py
@@ -1,41 +1,12 @@
 import streamlit as st
-from bs4 import BeautifulSoup
-import requests
-url="https://www.yardbarker.com/entertainment/articles/20_facts_you_might_not_know_about_pulp_fiction_112123/s1__35170955#slide_15"
-response=requests.get(url)
-soup=BeautifulSoup(response.content, "html.parser")
+
 import pandas as pd
 import random
 
 st.set_page_config(page_title="Random Facts", page_icon=":interrobang:", layout="wide")
 
-# WEB SCRAPPING
-#I personally DONT RECOMMEND what i am doing here: As you may know, some pages may change.
-# Pro Tip: Do the Web Scrapping, export it to a CSV and import it again with pandas. I am not doing it because I am sweet but a Psycho. 
 
-title=soup.find_all("h2")[1:]
-explan=soup.find_all("p")[1:21]
-images=soup.find_all("img")[15:35]
-links=[]
-for i in images:
-    try:
-        links.append(i["src"])
-    except:
-        links.append(i["data-src"])  
-
-# SOME ERROR HANDLING HERE: It was complicated to extract the links for the images that I will use for each film, so instead of copypasting them one by one as a uxui i mean as a rookie, I observed that the pattern was clear: src or data-src. Try and Except and it is done!
-
-quote=[]
-for i in title:
-    quote.append(i.text)
-
-text=[]
-for i in explan:
-    text.append(i.text)
-
-facts=pd.DataFrame({"Quote":quote, 
-                   "Text": text,
-                   "Links":links})
+facts=pd.read_csv("ws/Facts.csv")
 
 def get_random_quote():
     return facts.sample(1)
